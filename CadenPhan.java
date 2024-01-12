b462d25f87bff264f4da93d9091ec0e36a299a05
Caden Phan <caden.phan@mrjaffesclass.com>
2024-01-12 11:26:35 -0800

    initial commit

--- CadenPhan.java
+++ CadenPhan.java
@@ -14,4 +14,29 @@ public class CadenPhan extends Animal
   public String getType() {
     return "Turtle";
   }
+  /**
+   * What does the animal say?  Descendant classes should override this
+   * method or default speak will generate "Yaba-daba-doo"
+   */
+  public String speak() {
+    return "there are no accidents.";
+  }
+  /**
+   * This will return a String that describes the animal's general disposition.
+   * Sample dispositions could be "mean" or "friendly".  Use different adjectives
+   * in your method. The default disposition is "meh"
+   * 
+   * @return Disposition
+   */
+  public String getDisposition() {
+    return "Superkind";
+  }
+  /**
+   * This will return the animal's name. What's the default name?
+   * 
+   * @return The name of the animal (i.e. "Fido", "Rover")
+   */
+  public String getName() {
+    return "Oogway";
+  }
 }
